a14aa88ea5e20e0021cbf6f7ee6c1da5db40c9a5
Kartikay Dubey <Kartikayd2003@gmail.com>
2026-01-20 21:32:41 +0000

    disable buffer commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,8 +4,8 @@
 local map = vim.keymap.set
 map("n", "-", "<cmd>Neotree<CR>", { desc = "Start Neotree" })
 map("n", "<leader>rm", "<cmd>RemoteStart<CR>", { desc = "Start Remote" })
-map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
-map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
+-- map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
+-- map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
 map("n", "<M-x>", "<cmd>tabclose<CR>")
 map("n", "<M-c>", "<cmd>tabnew<CR>")
 map("n", "<M-p>", "<cmd>tabprevious<CR>")
